da9a7dc7f56fd4c3aa54a243af76051743e0ce98
Motya22 <motya2222@gmail.com>
2023-06-17 09:20:59 +0300

    Complete project
    1. Install the repository for this project;
    2. Create a web page with a 16x16 grid of square div's;
    3. Set the "hover" effect so that the grid divs change color when the mouse passes over them, leaving a (pixelated) trail in the grid, like a pen;
    4. Add a button at the top of the screen that will send the user a popup asking for the number of squares per side for the new grid;
    5.1 Each interaction should randomize the square’s RGB value entirely;
    5.2 Implement a gradual darkening effect, where each interaction adds 10% more black or color to the square.

--- script.js
+++ script.js
@@ -1,1 +1,63 @@
-console.log('Hello World!');
+const grid = document.querySelector('.grid');
+const changeBtn = document.querySelector('#change');
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function makeGrid(rows = 16, columns = 16) {
+  for (let i = 0; i < rows * columns; i++) {
+    const cell = document.createElement('div');
+
+    cell.classList.add('grid__item');
+    cell.style.flex = `0 0 ${100 / rows}%`;
+
+    let alphaPersent = 0;
+
+    cell.addEventListener('mouseenter', function () {
+      const redNumberRgb = getRandomIntInclusive(0, 255);
+      const greenNumberRgb = getRandomIntInclusive(0, 255);
+      const blueNumberRgb = getRandomIntInclusive(0, 255);
+
+      if (alphaPersent < 100) alphaPersent += 10;
+
+      this.style.backgroundColor = `rgba(${redNumberRgb}, ${greenNumberRgb}, ${blueNumberRgb}, ${
+        alphaPersent / 100
+      })`;
+    });
+    cell.addEventListener('mouseleave', function () {
+      this.style.backgroundColor = 'transparent';
+    });
+
+    grid.appendChild(cell);
+  }
+}
+
+function changeDimensions() {
+  let dimensions = +prompt(
+    'How many squares do you want per side (max 100)?',
+    16
+  );
+  console.log(dimensions);
+
+  if (dimensions !== 0 && !window.isNaN(dimensions)) {
+    grid.innerHTML = '';
+
+    makeGrid(dimensions, dimensions);
+  } else {
+    do {
+      dimensions = +prompt('Please choose a number of squares below 100:', 16);
+    } while (dimensions === 0 || window.isNaN(dimensions));
+
+    grid.innerHTML = '';
+
+    makeGrid(dimensions, dimensions);
+  }
+}
+
+changeBtn.addEventListener('click', changeDimensions);
+
+makeGrid();
